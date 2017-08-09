7e44ffacef23dfd674151a5dfa6495af9135692c
Alexander Bogushov <abogushov@gmail.com>
2017-08-09 20:19:34 +0500

    Release 0.1.2.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 import os
-from setuptools import find_packages, setup
+import re
+from pathlib import Path
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
+
+with Path(__file__).with_name('django_admin_json_editor').joinpath('__init__.py').open() as f:
+    VERSION = re.compile(r'.*__version__ = \'(.*?)\'', re.S).match(f.read()).group(1)
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-json-editor',
-    version='0.1.1',
+    version=VERSION,
     packages=['django_admin_json_editor'],
     include_package_data=True,
     license='MIT License',
