62cd196a05a036ea4750bfccf613679ee2680f64
Alexander Bogushov <abogushov@gmail.com>
2017-11-25 16:35:02 +0500

    🔨 Use pathlib in setup.py.

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ import re
 from pathlib import Path
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
 
-with Path(__file__).with_name('django_admin_json_editor').joinpath('__init__.py').open() as f:
-    VERSION = re.compile(r'.*__version__ = \'(.*?)\'', re.S).match(f.read()).group(1)
+project_path = Path(__file__).parent
+
+README = (project_path / 'README.md').read_text()
+
+version_text = (project_path / 'django_admin_json_editor' / '__init__.py').read_text()
+VERSION = re.compile(r'.*__version__ = \'(.*?)\'', re.S).match(version_text).group(1)
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(project_path)
 
 setup(
     name='django-admin-json-editor',
